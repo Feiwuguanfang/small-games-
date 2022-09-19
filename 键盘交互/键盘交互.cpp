e3aed6268c9368979f1c926d6c0a850ae6e005c3
Feiwuguanfang <110074865+Feiwuguanfang@users.noreply.github.com>
2022-09-19 16:31:39 +0800

    Add files via upload

---
+++ 键盘交互/键盘交互.cpp
@@ -0,0 +1,42 @@
+﻿#include<stdio.h>
+#include<easyx.h>
+#include<conio.h>
+int main()
+{
+	initgraph(800, 600);
+	setorigin(400, 300);
+	setaspectratio(1, -1);
+	setbkcolor(RGB(164, 225, 202));
+	cleardevice();
+	setfillcolor(GREEN);
+	setpolyfillmode(WINDING);
+	int x = 0; int y = 0;
+	solidcircle(x, y, 50);
+	while (1)
+	{
+		char c = _getch();
+		switch (c)
+		{
+		case'w':
+			y = y + 50;
+			break;
+		case's':
+			y = y - 50;
+			break;
+		case'a':
+			x = x - 50;
+			break;
+		case'd':
+			x = x + 50;
+			break;
+		}
+		cleardevice();
+		solidcircle(x, y, 50);
+	}
+	getchar();
+	closegraph();
+	
+	return 0;
+
+
+}
